720a657336b681f2c11ada65053bccd0f9b39806
Andrei <litkov99@mail.ru>
2026-08-30 18:49:14 +0300

    Completed HW

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,19 +1,24 @@
 package org.skypro.skyshop.product;
 
-public class Product {
+public abstract class Product {
     private final String name;
-    private final int price;
 
-    public Product(String name, int price) {
+    public Product(String name) {
         this.name = name;
-        this.price = price;
     }
 
-    public int getPrice() {
-        return price;
-    }
+    public abstract int getPrice();
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + ": " + getPrice();
+    }
+
+    public boolean isSpecial() {
+        return false;
     }
 }
